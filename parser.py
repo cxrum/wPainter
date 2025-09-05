de8f299e96b1986510479df87f4921dd21f5d3e8
Denis Kostiuk <64838929+Chrumba@users.noreply.github.com>
2025-09-05 12:16:23 +0300

    Color detecting improvement

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import time
 
 import numpy
 import cv2 as cv
+import numpy as np
 from PIL import ImageGrab
 
 from main import MASK, RESULT_PATH, DETECTED_RECTS
@@ -12,6 +13,8 @@ from utils import Color, PixelNode, Point
 def save_image(path, img):
     cv.imwrite(str(path), img)
 
+def _is_square_fit(area, avg_area, rects_areas) -> bool:
+    return min(rects_areas) <= area <= avg_area
 
 class PixelParser:
 
@@ -27,10 +30,10 @@ class PixelParser:
 
         return res
 
-    def points_for_color(self, _color: Color, tolerance = 20) -> list[Point]:
+    def points_for_color(self, _color: Color, tolerance = 15) -> list[Point]:
         res: list[Point] = []
 
-        dst = cv.cvtColor(self.img, cv.COLOR_BGRA2BGR)
+        dst = cv.cvtColor(self.img, cv.COLOR_RGB2BGR)
 
         lower = numpy.array([
             max(0, _color.b - tolerance),
@@ -51,7 +54,8 @@ class PixelParser:
         if MASK:
             save_image(RESULT_PATH / f"{_color.r} {_color.g} {_color.b} mask.png", mask)
 
-        rects = []
+        avg_area  = 0.0
+        rects_areas = []
 
         for c in cnts:
             peri = cv.arcLength(c, True)
@@ -59,12 +63,18 @@ class PixelParser:
             if len(approx) == 4:
                 x, y, w, h = cv.boundingRect(approx)
 
-                if [w, h] not in rects:
-                    rects.append([w, h])
+                if w*h not in rects_areas:
+                    rects_areas.append(w*h)
+                    _sum = 0
+                    for area in rects_areas:
+                        _sum += area
+                    avg_area = _sum / len(rects_areas)
 
-                if (w < 10 and h < 10):
+                if _is_square_fit(w*h, avg_area, rects_areas):
                     cv.rectangle(dst, (x, y), (x + w, y + h), (0, 0, 255), 1)
                     res.append(Point(x+int(w//2), y+int(h//2)))
+
+        print(rects_areas)
 
         if DETECTED_RECTS:
             save_image(RESULT_PATH / f"{_color.r} {_color.g} {_color.b} res.png", dst)
