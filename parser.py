40b4680e11f174e85ee2717a6c6d7562846e46da
Denis Kostiuk <64838929+Chrumba@users.noreply.github.com>
2025-09-10 23:36:49 +0300

    Improved rect detecting

--- parser.py
+++ parser.py
@@ -4,31 +4,52 @@ import cv2 as cv
 import numpy
 
 from main import MASK, RESULT_PATH, DETECTED_RECTS
-from utils import Color, PixelNode, Point
+from utils import Color, PixelNode, Point, ColorPixel, Rect, Area
 
 
 def save_image(path, img):
     cv.imwrite(str(path), img)
 
-def _is_square_fit(area, rects_areas, k=0.8) -> bool:
-    return True
+def _is_square_fit(area, rects_areas, k=0.5) -> bool:
+    avg = statistics.mean(rects_areas)
+    _max = avg * (1 - k)
+    return min(rects_areas) <= area <= _max
 
 class PixelParser:
 
-    def __init__(self, img):
+    def __init__(self, img, area: Area):
         self.img = img
+        self.rects_areas = []
+        self.area = area
 
-    def nodes_for_colors(self, colors: list[Color]) -> list[PixelNode]:
-        res: list[PixelNode] = []
+
+    def matrix_points_parse(self, colors: list[Color]) -> list[ColorPixel]:
+        all_pixels: list[ColorPixel] = []
 
         for _color in colors:
-            points = self.points_for_color(_color)
-            res.append(PixelNode(_color, points))
+            rects = self.parse_points_for_color(_color, rect_checking=False)
+            for rect in rects:
+                area = rect.w * rect.h
+                if area not in self.rects_areas:
+                    self.rects_areas.append(area)
 
-        return res
+        self.rects_areas.sort()
+        print(self.rects_areas)
 
-    def points_for_color(self, _color: Color, tolerance = 0) -> list[Point]:
-        res: list[Point] = []
+        for _color in colors:
+            rects = self.parse_points_for_color(_color)
+
+            for rect in rects:
+                all_pixels.append(ColorPixel(_color, Point(rect.x, rect.y), rect.w, rect.h))
+
+            all_pixels.sort(key=lambda px: (px.point.y, px.point.x))
+
+        return all_pixels
+
+
+
+    def parse_points_for_color(self, _color: Color, tolerance = 5, rect_checking = True) -> list[Rect]:
+        res: list[Rect] = []
 
         dst = cv.cvtColor(self.img, cv.COLOR_RGB2BGR)
 
@@ -51,29 +72,19 @@ class PixelParser:
         if MASK:
             save_image(RESULT_PATH / f"{_color.r} {_color.g} {_color.b} mask.png", mask)
 
-        rects_areas = []
-
         for c in cnts:
             peri = cv.arcLength(c, True)
             approx = cv.approxPolyDP(c, 0.15 * peri, True)
             if len(approx) == 4:
                 x, y, w, h = cv.boundingRect(approx)
 
-                if w*h not in rects_areas:
-                    rects_areas.append(w*h)
-
-        for c in cnts:
-            peri = cv.arcLength(c, True)
-            approx = cv.approxPolyDP(c, 0.15 * peri, True)
-            if len(approx) == 4:
-                x, y, w, h = cv.boundingRect(approx)
-
-                if _is_square_fit(w*h, rects_areas):
-                    cv.rectangle(dst, (x, y), (x + w, y + h), (0, 0, 255), 1)
-                    res.append(Point(x+int(w//2), y+int(h//2)))
-                    print(w*h)
-
-
+                if self.area.is_entry(x, y):
+                    if rect_checking:
+                        if _is_square_fit(w*h, self.rects_areas) :
+                            cv.rectangle(dst, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                            res.append(Rect(x+int(w//2), y+int(h//2), w, h))
+                    else:
+                        res.append(Rect(x + int(w // 2), y + int(h // 2), w, h))
 
         if DETECTED_RECTS:
             save_image(RESULT_PATH / f"{_color.r} {_color.g} {_color.b} res.png", dst)
